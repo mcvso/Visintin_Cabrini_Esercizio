4e9f794b9cd4c1baf791321ebd78f01da8b19b31
Vecchielia <michele.visintin1@isitgo.it>
2022-12-21 10:06:47 +0100

    main

--- Visintin_Cabrini_Esercizio/src/visintin_cabrini_esercizio/Visintin_Cabrini_Esercizio.java
+++ Visintin_Cabrini_Esercizio/src/visintin_cabrini_esercizio/Visintin_Cabrini_Esercizio.java
@@ -1,9 +1,25 @@
 package visintin_cabrini_esercizio;
 
+import java.util.Scanner;
+
 public class Visintin_Cabrini_Esercizio {
 
     public static void main(String[] args) {
-
+        
+        String nome;
+        float estensione;
+        Scanner input=new Scanner(System.in);
+        
+        
+        System.out.println("inserire il nome della città");
+        nome=input.nextLine();
+        System.out.println("inserire l'estensione della città");
+        estensione=input.nextFloat();
+        Citta citta=new Citta(nome,estensione);
+        
+        System.out.println(citta.toString());
+        
+        
     }
     
 }
